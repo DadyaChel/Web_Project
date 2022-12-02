0d808e30240375d07b50096b4f2fec05e1ff5727
DadyaChel <kvvvs04@gmail.com>
2022-12-02 15:15:24 +0300

    First commit

---
+++ shop/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=30, verbose_name='ФИО клиента')
+    price = models.IntegerField(verbose_name='цена товара')
+
+    def __str__(self):
+        return self.name
+
+
+class Purchase(models.Model):
+    name = models.CharField(max_length=30, verbose_name='ФИО клиента')
+    age = models.IntegerField(verbose_name='возраст клиента')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='purchases')
+    date_purchase = models.DateField(verbose_name='Дата покупки')
+
+    def __str__(self):
+        return self.name
